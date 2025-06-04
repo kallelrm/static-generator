4eaf319edb9b83c127d4ca3a7823ed9e6439f57b
tivrusky <kallelr@outlook.com>
2025-06-04 03:29:03 -0300

    adding block_to_blocktype function

--- src/blocks.py
+++ src/blocks.py
@@ -1,4 +1,15 @@
 import re
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED = "unordered_list"
+    ORDERED = "ordered_list"     
+
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split('\n\n')
     aux_blocks = []
@@ -7,19 +18,26 @@ def markdown_to_blocks(markdown):
             splitted = re.split(r'\n( )*', block)
             if ' ' in splitted:
                 splitted.pop(splitted.index(' '))
-            print('SPLITTED', splitted)
             aux_blocks.append('\n'.join(splitted).strip())
             continue
         aux_blocks.append(block.strip('\n '))
-        
-    # for block in aux_blocks:
-    #     if '\n' in block:
-    #         splitted = re.split(r'\n( )*', block)
-    #         if ' ' in splitted:
-    #             splitted.pop(splitted.index(' '))
-    #         print('SPLITTED', splitted)
-    #         '\n'.join(splitted)
-        
-         
 
-    print(aux_blocks)
+    return aux_blocks
+
+def block_to_blocktype(markdown):
+    stripped = markdown.strip()
+
+    if re.match(r'^#{1,6} ', markdown):
+        return BlockType.HEADING
+    if markdown.startswith('```') and markdown.endswith('```'):
+        return BlockType.CODE
+    if re.match(r'^(\n?>)', markdown, flags=re.MULTILINE):
+        return BlockType.QUOTE
+    if re.match(r'^(\n?-)', markdown, flags=re.MULTILINE):
+        return BlockType.UNORDERED
+    if re.match(r'^(\n?\d+\.)', markdown, flags=re.MULTILINE):
+        return BlockType.ORDERED
+    return BlockType.PARAGRAPH
+
+def markdown_to_html(markdown):
+    pass
--- src/test_blocks.py
+++ src/test_blocks.py
@@ -1,5 +1,6 @@
 import unittest
-from blocks import markdown_to_blocks
+import textwrap
+from blocks import *
 
 class TestBlock(unittest.TestCase):
 
@@ -14,14 +15,102 @@ class TestBlock(unittest.TestCase):
     - with items
     """
             blocks = markdown_to_blocks(md)
-        #     self.assertEqual(
-        #         blocks,
-        #         [
-        #             "This is **bolded** paragraph",
-        #             "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
-        #             "- This is a list\n- with items",
-        #         ],
-        #    )
+            self.assertEqual(
+                blocks,
+                [
+                    "This is **bolded** paragraph",
+                    "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+                    "- This is a list\n- with items",
+                ],
+           )
+    
+    def test_block_to_blocktype_code(self):
+        md = """```
+        code
+        code
+        code
+        ```"""
+        blocktype = block_to_blocktype(md)
+        self.assertEqual(BlockType.CODE, blocktype)
 
+    def test_block_to_blocktype_heading1(self):
+        md = textwrap.dedent("""
+        # a
+        """).strip()
+        blocktype = block_to_blocktype(md)
+        self.assertEqual(BlockType.HEADING, blocktype)
+    
+    def test_block_to_blocktype_heading2(self):
+        md = textwrap.dedent("""
+        ## a
+        """).strip()
+        blocktype = block_to_blocktype(md)
+        self.assertEqual(BlockType.HEADING, blocktype)
+
+    def test_block_to_blocktype_heading3(self):
+        md = textwrap.dedent("""
+        ### a
+        """).strip()
+        blocktype = block_to_blocktype(md)
+        self.assertEqual(BlockType.HEADING, blocktype)
+
+    def test_block_to_blocktype_heading4(self):
+        md = textwrap.dedent("""
+        #### a
+        """).strip()
+        blocktype = block_to_blocktype(md)
+        self.assertEqual(BlockType.HEADING, blocktype)
+
+    def test_block_to_blocktype_heading5(self):
+        md = textwrap.dedent("""
+        ##### a
+        """).strip()
+        blocktype = block_to_blocktype(md)
+        self.assertEqual(BlockType.HEADING, blocktype)
+
+    def test_block_to_blocktype_heading6(self):
+        md = textwrap.dedent("""
+        ###### a
+        """).strip()
+        blocktype = block_to_blocktype(md)
+        self.assertEqual(BlockType.HEADING, blocktype)
+
+    def test_block_to_blocktype_quote(self):
+        md = textwrap.dedent("""
+        > quote1
+        > quote2
+        > quote3
+        """).strip()
+        blocktype = block_to_blocktype(md)
+        self.assertEqual(BlockType.QUOTE, blocktype)
+
+    def test_block_to_blocktype_unordered(self):
+        md = textwrap.dedent("""
+        - lista
+        - lista
+        - lista 
+        """).strip()
+        blocktype = block_to_blocktype(md)
+        self.assertEqual(BlockType.UNORDERED, blocktype)
+    
+    def test_block_to_blocktype_ordered(self):
+        md = textwrap.dedent("""
+        1. lista
+        2. lista
+        3. lista 
+        """).strip()
+        blocktype = block_to_blocktype(md)
+        self.assertEqual(BlockType.ORDERED, blocktype)
+
+    def test_block_to_blocktype_paragraph(self):
+        md = textwrap.dedent("""
+        um paragrafo qualquer sobre
+        uma pessoa que eu amo muito
+        e todo meu amor há de transbordar
+        pelo mundo
+        """).strip()
+
+        blocktype = block_to_blocktype(md)
+        self.assertEqual(BlockType.PARAGRAPH, blocktype)
 if __name__ == '__main__':
     unittest.main()
