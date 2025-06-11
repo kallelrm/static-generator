c2a47b141c0cae2b9c3a00dbaad3392332f4c0a1
tivrusky <kallelr@outlook.com>
2025-06-11 19:46:40 -0300

    last fixes

--- src/main.py
+++ src/main.py
@@ -2,7 +2,8 @@ import sys
 from file import copier
 from helpers import generate_page_recursive
 def main():
-    basepath = sys.argv
+    basepath = sys.argv[1] if len(sys.argv) > 1 else '/' 
+    print(basepath)
     copier('static', 'docs')
     generate_page_recursive("content", "template.html", "docs", basepath)
 
